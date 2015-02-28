5050198689227650bc422231303793bbab7a1cc0
Raphael Ramos <contato@raphaelramos.com.br>
2015-02-28 17:39:37 -0300

    vr 0.2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 README
+
+Want to save the data sended by your WPCF7 forms? That's the goal of this plugin. It's create a Custom Post Type **form2post** for save the data.
+
+The email sended is daved as *post content* and all the form fields are saved as metadata.
+
+### TO-DO
+
+* Shortcodes for list the data saved, by FORM ( something like [f2p form_id="0"]
--- form-2-post.php
+++ form-2-post.php
@@ -3,32 +3,36 @@
 /*
 Plugin Name: Form 2 Post
 Plugin URI: http://www.raphaelramos.com.br/wp/plugins/form-2-post/
-Description: Plugin que salva dados enviados através dos formulários do WPCF7 como um post.
+Description: Store WPCF7 posted data as post.
 Author: Raphael Ramos
 Author URI: http://www.raphaelramos.com.br/
 Text Domain: form-2-post
 Domain Path: /lang/
-Version: 0.1
+Version: 0.2
+Date: 2015-02-28
 */
 
-	/*
-	 *	Updated: 2015-02-28
-	 *==============================*/
-
 	class Form_2_Post {
-		
+
+		// post name
 		private $cpt = 'form2post';
-		
+
 		function __construct(){
-			
-			// 
+
+			// deal with form submit
 			add_action( 'wpcf7_before_send_mail', array( $this, 'data' ) );
-			
+
+			// create cpt for store data
 			add_action('init', array( $this, 'cpt' ) );
 		}
-	
+
+		
+		/***
+		 *	Deal with posted data
+		 *==============================*/
 		public function data( $cf7 ){
 
+			// posted data
 			$form_data = $_POST;
 			unset(
 				$form_data[ "_wpcf7" ]
@@ -38,15 +42,18 @@ Version: 0.1
 				, $form_data[ "_wpnonce" ]
 			);
 
+			// user data 
 			$user = wp_get_current_user();
-			
+
+			// mail data
 			$mail = WPCF7_Mail::send( $cf7->prop( 'mail' ), 'mail', false );
-			
-			$submit = WPCF7_Submission::get_instance();
-			$files = $submit->uploaded_files();
-			
 			$body = $mail[ 'body' ];
 
+			// files data
+			$submit = WPCF7_Submission::get_instance();
+			$files = $submit->uploaded_files();
+
+			// post data for saving
 			$post = array(
 				'post_author'		=> $user->ID
 				, 'post_type' 		=> $this->cpt
@@ -58,18 +65,22 @@ Version: 0.1
 				, 'ping_status' 	=> 'closed' // Disable pingbacks
 			);
 
+			// create post
 			$postID = wp_insert_post( $post, true );
 
-			if( is_a( $postID, 'WP_Error' ) ) return $postID->get_error_message();
+			// check for error
+			if( is_wp_error( $postID ) ) return $postID->get_error_message();
 			
+			// deal with files
 			if( count( $files ) ){
 				$data_file = array();
 				$body .= "\n\n--\n";
 				foreach( $files as $k => $file ){
-					$upload = $this->_media( $file, $postID );
-					$form_data[ $k ] = $upload;
-					$data_file[] = array( 'name' => $k, 'src' => $upload );
-					$body .= "{$k}: <a href='{$upload}'>{$upload}</a>\n";
+					if( $upload = $this->_media( $file, $postID ) ){
+						$form_data[ $k ] = $upload;
+						$data_file[] = array( 'name' => $k, 'src' => $upload );
+						$body .= "{$k}: <a href='{$upload}'>{$upload}</a>\n";
+					}
 				}
 				$form_data[ 'files' ] = json_encode( $data_file );
 				
@@ -81,19 +92,17 @@ Version: 0.1
 				);
 				wp_update_post( $post );
 			}
-			
-			echo "<pre>\n";
-			var_dump( $form_data );
-			echo "</pre>\n";
 
+			// save fields as post meta
 			foreach( $form_data as $key => $val ){
 				add_post_meta( $postID, $key, $val, true ) or update_post_meta( $postID, $key, $val );
 			}
-
-			#var_dump( $cf7->posted_data );
-			#$cf7->skip_mail = true;
 		}
-	
+
+
+		/***
+		 *	Deal with media files
+		 *==============================*/
 		private function _media( $file, $pid ){
 			$filename = basename( $file );
 			$testtype = wp_check_filetype_and_ext( $file, $filename, null );
@@ -102,51 +111,54 @@ Version: 0.1
 			if( $testtype[ 'proper_filename' ] )
 				$filename = $testtype[ 'proper_filename' ];
 
+			// need check this
 			#if( ( !$testtype[ 'type' ] || !$testtype[ 'ext' ]) && !current_user_can( 'unfiltered_upload' ) )
 				#return __('Sorry, this file type is not permitted for security reasons.');
 
 			if( !$testtype[ 'ext' ] )
 				$testtype[ 'ext' ] = ltrim( strrchr( $filename, '.' ), '.' );
-			
+
 			// Check if the uploads directory exists/create it.  If it fails, the parent directory probably isn't writable.
 			if( !( $uploads = wp_upload_dir( null ) ) )
-				return $uploads[ 'error' ];
+				// return $uploads[ 'error' ];
+				return false;
 
 			// adjust file name for unique name
 			$filename = wp_unique_filename( $uploads[ 'path' ], $filename );			
-			
+
 			// full ath of new file
 			$new_file = $uploads[ 'path' ] .'/'. $filename;
-			
+
 			// Copy the uploaded file to the correct uploads folder.
 			if( false === @copy( $file, $new_file ) )
-				return sprintf( __('The file %s could not be moved to %s.'), $file, $new_file );
+				// return sprintf( __('The file %s could not be moved to %s.'), $file, $new_file );
+				return false;
 
 			// Set correct file permissions
 			$stat = stat( dirname( $new_file ) );
 			$perms = $stat[ 'mode' ] & 0000666;
 			@chmod( $new_file, $perms );
-			
+
+			// Add the attachment
 			$attachment = array(
 				'post_mime_type'    => $testtype[ 'type' ],
 				'post_title'        => preg_replace( '/\.[^.]+$/', '', $filename ),
 				'post_content'      => '',
 				'post_status'       => 'inherit',
 			);
-			// Add the attachment
-			$att_id = wp_insert_attachment( $attachment, $new_file, $pid );
+			if( $att_id = wp_insert_attachment( $attachment, $new_file, $pid ) )
 			
-			// Test for an image and only perform the rest if the upload is an image...
-			if( @getimagesize( $new_file ) !== false ){
-				
-				require_once ABSPATH .'wp-admin/includes/image.php';
-				
-				$att_data = wp_generate_attachment_metadata( $att_id, $new_file );
-				wp_update_attachment_metadata( $att_id, $att_data );
+				// Test for an image and only perform the rest if the upload is an image...
+				if( @getimagesize( $new_file ) !== false ){
+					require_once ABSPATH .'wp-admin/includes/image.php';
+					$att_data = wp_generate_attachment_metadata( $att_id, $new_file );
+					wp_update_attachment_metadata( $att_id, $att_data );
+				}
+			
+				// return true;
+				return $uploads[ 'baseurl' ] .'/'. $att_data[ 'file' ];
 			}
-			
-			// return true;
-			return $uploads[ 'baseurl' ] .'/'. $att_data[ 'file' ];
+			return false;
 		}
 	
 		public function cpt(){
