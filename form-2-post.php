dc32373292f73131313a546da4073e920b586587
Raphael Ramos <contato@raphaelramos.com.br>
2015-05-02 01:33:35 -0300

    vr 0.3

--- form-2-post.php
+++ form-2-post.php
@@ -8,8 +8,8 @@ Author: Raphael Ramos
 Author URI: http://www.raphaelramos.com.br/
 Text Domain: form-2-post
 Domain Path: /lang/
-Version: 0.27
-Date: 2015-05-01
+Version: 0.3
+Date: 2015-05-02
 */
 
 	class Form_2_Post {
@@ -24,6 +24,9 @@ Date: 2015-05-01
 
 			// create cpt for store data
 			add_action( 'init', array( $this, 'cpt' ) );
+			
+			// shortcode
+			add_shortcode( "f2p", array( $this, 'shortcode' ) );
 		}
 
 		
@@ -44,20 +47,13 @@ Date: 2015-05-01
 
 			// user data 
 			$user = wp_get_current_user();
-
-			// mail data => changed in 2015-05-01
-			#$mail = WPCF7_Mail::send( $cf7->prop( 'mail' ), 'mail', false );
-			#$body = $mail[ 'body' ];
 			
+			// mail body
 			$mail = $cf7->prop( 'mail' );
 			if( function_exists( 'all_fields_wpcf7_before_send_mail' ) ){
 				$mail = all_fields_wpcf7_before_send_mail( $mail );
 			}
 			$body = wpcf7_mail_replace_tags( $mail[ 'body' ], $cf7 );
-
-			// files data
-			$submit = WPCF7_Submission::get_instance();
-			$files = $submit->uploaded_files();
 
 			// post data for saving
 			$post = array(
@@ -77,10 +73,12 @@ Date: 2015-05-01
 			// check for error
 			if( is_wp_error( $postID ) ) return $postID->get_error_message();
 			
+			
+			// files data
+			$submit = WPCF7_Submission::get_instance();
+			$files = $submit->uploaded_files();
+			
 			// deal with files
-			
-			var_dump( $files );
-			
 			if( count( $files ) ){
 				$data_file = array();
 				$body .= "\n\n------------------------------\n";
@@ -92,8 +90,7 @@ Date: 2015-05-01
 					}
 				}
 				$form_data[ 'files' ] = json_encode( $data_file );
-				
-				
+
 				// update post content with files links
 				$post = array(
 					'ID' => $postID,
@@ -115,8 +112,6 @@ Date: 2015-05-01
 		private function _media( $file, $pid ){
 			$filename = basename( $file );
 			$testtype = wp_check_filetype_and_ext( $file, $filename, null );
-			
-			#var_dump( array( '_media::$testtype' => $testtype ) );
 
 			// Check if a proper filename was given for in incorrect filename and use it instead
 			if( $testtype[ 'proper_filename' ] )
@@ -125,7 +120,6 @@ Date: 2015-05-01
 			// need check this
 			if( ( !$testtype[ 'type' ] || !$testtype[ 'ext' ]) && !current_user_can( 'unfiltered_upload' ) ){
 				#return __( 'Sorry, this file type is not permitted for security reasons.' );
-				#var_dump( array( '_media::$testtype::error' => 'Sorry, this file type is not permitted for security reasons.' ) );
 				return false;
 			}
 
@@ -135,24 +129,18 @@ Date: 2015-05-01
 			// Check if the uploads directory exists/create it.  If it fails, the parent directory probably isn't writable.
 			if( !( $uploads = wp_upload_dir( null ) ) ){
 				// return $uploads[ 'error' ];
-				#var_dump( array( '_media::wp_upload_dir::error' => $uploads[ 'error' ] ) );
 				return false;
 			}
 
 			// adjust file name for unique name
 			$filename = wp_unique_filename( $uploads[ 'path' ], $filename );
-			
-			#var_dump( array( '_media::$filename' => $filename ) );
 
 			// full ath of new file
 			$new_file = $uploads[ 'path' ] .'/'. $filename;
-			
-			#var_dump( array( '_media::$new_file ' => $new_file  ) );
 
 			// Copy the uploaded file to the correct uploads folder.
 			if( false === @copy( $file, $new_file ) ){
 				// return sprintf( __('The file %s could not be moved to %s.'), $file, $new_file );
-				#var_dump( array( '_media::copy::error ' => sprintf( __('The file %s could not be moved to %s.'), $file, $new_file ) ) );
 				return false;
 			}
 
@@ -168,6 +156,7 @@ Date: 2015-05-01
 				'post_content'      => '',
 				'post_status'       => 'inherit',
 			);
+			
 			if( $att_id = wp_insert_attachment( $attachment, $new_file, $pid ) ){
 			
 				// Test for an image and only perform the rest if the upload is an image...
@@ -175,9 +164,6 @@ Date: 2015-05-01
 					require_once ABSPATH .'wp-admin/includes/image.php';
 					$att_data = wp_generate_attachment_metadata( $att_id, $new_file );
 					wp_update_attachment_metadata( $att_id, $att_data );
-			
-					// return true;
-					#return $uploads[ 'baseurl' ] .'/'. $att_data[ 'file' ];
 				}
 				
 				return wp_get_attachment_url( $att_id );
@@ -207,6 +193,54 @@ Date: 2015-05-01
 				'supports'          => array( 'title', 'editor', 'author', 'custom-fields', 'revisions' ),
 			) );
 		}
+		
+		public function shortcode( $atts ){
+			extract( shortcode_atts( array(
+				'form_id' => 0
+				, 'html_class' => ''
+				, 'html_id' => ''
+			), $atts ) );
+			
+			$parent = get_post( $form_id );
+			
+			if( $html_class != '' ) $html_class = ' '. trim( $html_class );
+			
+			if( $html_id != '' ) $html_id = ' id="'. trim( $html_id ) .'"';
+			
+			$count = 0;
+			
+			$data = array();
+			
+			$html = array(
+				'<div'. $html_id .' class="f2p-viewer row'. $html_class .'">'
+					,'<h3>Formulário: '. $parent->post_title .'</h3>'
+					,'<div class="f2p-list col col-1of3">'
+					,'<ul>'
+			);
+			
+			$q = new WP_Query( array( 'post_type' => $this->cpt, 'posts_per_page' => '-1', 'post_parent' => $form_id, 'post_status' => 'any' ) );
+			if( $q->have_posts() ){
+				while( $q->have_posts() ){
+					$q->the_post();
+					$html[] = '<li><a href="#" data-f2p-id="'. get_the_ID() .'">'. get_field( 'nome' ) .' ('. get_field( 'mail' ) .') <span>'. get_the_date( 'd-m-Y H:i:s' ) .'</span></a></li>';
+					$content = apply_filters( 'the_content', get_the_content( '' ) );
+					$data[] = array( 'id' => get_the_ID(), 'body' => $content, 'fields' => get_post_custom() );
+				}
+				wp_reset_postdata();
+			}
+			
+			$html[] = '</ul>';
+			$html[] = '</div>';
+			$html[] = '<div class="col col-2of3"><div class="f2p-content"></div></div>';
+			$html[] = '</div>';
+			$html[] = '<script>';
+			$html[] = 'var $f2p_data = '. json_encode( array( 'count' => count( $data ), 'data' => $data ) ) .';';
+			$html[] = '</script>';
+			
+			return implode( "\n", $html );
+			
+		}
+
 	}
 	
 	$f2p = new Form_2_Post;
