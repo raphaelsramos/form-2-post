82953bf60e3b93c6a6192ed5483a499fac277d9b
Raphael Ramos <contato@raphaelramos.com.br>
2015-05-01 23:42:16 -0300

    vr 0.27

--- form-2-post.php
+++ form-2-post.php
@@ -8,8 +8,8 @@ Author: Raphael Ramos
 Author URI: http://www.raphaelramos.com.br/
 Text Domain: form-2-post
 Domain Path: /lang/
-Version: 0.22
-Date: 2015-02-28
+Version: 0.27
+Date: 2015-05-01
 */
 
 	class Form_2_Post {
@@ -23,7 +23,7 @@ Date: 2015-02-28
 			add_action( 'wpcf7_before_send_mail', array( $this, 'data' ) );
 
 			// create cpt for store data
-			add_action('init', array( $this, 'cpt' ) );
+			add_action( 'init', array( $this, 'cpt' ) );
 		}
 
 		
@@ -45,9 +45,15 @@ Date: 2015-02-28
 			// user data 
 			$user = wp_get_current_user();
 
-			// mail data
-			$mail = WPCF7_Mail::send( $cf7->prop( 'mail' ), 'mail', false );
-			$body = $mail[ 'body' ];
+			// mail data => changed in 2015-05-01
+			#$mail = WPCF7_Mail::send( $cf7->prop( 'mail' ), 'mail', false );
+			#$body = $mail[ 'body' ];
+			
+			$mail = $cf7->prop( 'mail' );
+			if( function_exists( 'all_fields_wpcf7_before_send_mail' ) ){
+				$mail = all_fields_wpcf7_before_send_mail( $mail );
+			}
+			$body = wpcf7_mail_replace_tags( $mail[ 'body' ], $cf7 );
 
 			// files data
 			$submit = WPCF7_Submission::get_instance();
@@ -58,7 +64,7 @@ Date: 2015-02-28
 				'post_author'		=> $user->ID
 				, 'post_type' 		=> $this->cpt
 				, 'post_title' 		=> $cf7->title() .' - '. date( 'Y-m-d H:i:s' )
-				, 'post_content' 	=> $mail[ 'body' ] // Store the email body as the post content
+				, 'post_content' 	=> $body // Store the email body as the post content
 				, 'post_parent' 	=> $cf7->id()
 				, 'post_status' 	=> 'pending' // Set the default post status to 'pending'
 				, 'comment_status' 	=> 'closed' // Disable comments
@@ -72,9 +78,12 @@ Date: 2015-02-28
 			if( is_wp_error( $postID ) ) return $postID->get_error_message();
 			
 			// deal with files
+			
+			var_dump( $files );
+			
 			if( count( $files ) ){
 				$data_file = array();
-				$body .= "\n\n--\n";
+				$body .= "\n\n------------------------------\n";
 				foreach( $files as $k => $file ){
 					if( $upload = $this->_media( $file, $postID ) ){
 						$form_data[ $k ] = $upload;
@@ -106,33 +115,46 @@ Date: 2015-02-28
 		private function _media( $file, $pid ){
 			$filename = basename( $file );
 			$testtype = wp_check_filetype_and_ext( $file, $filename, null );
+			
+			#var_dump( array( '_media::$testtype' => $testtype ) );
 
 			// Check if a proper filename was given for in incorrect filename and use it instead
 			if( $testtype[ 'proper_filename' ] )
 				$filename = $testtype[ 'proper_filename' ];
 
 			// need check this
-			#if( ( !$testtype[ 'type' ] || !$testtype[ 'ext' ]) && !current_user_can( 'unfiltered_upload' ) )
-				#return __('Sorry, this file type is not permitted for security reasons.');
+			if( ( !$testtype[ 'type' ] || !$testtype[ 'ext' ]) && !current_user_can( 'unfiltered_upload' ) ){
+				#return __( 'Sorry, this file type is not permitted for security reasons.' );
+				#var_dump( array( '_media::$testtype::error' => 'Sorry, this file type is not permitted for security reasons.' ) );
+				return false;
+			}
 
 			if( !$testtype[ 'ext' ] )
 				$testtype[ 'ext' ] = ltrim( strrchr( $filename, '.' ), '.' );
 
 			// Check if the uploads directory exists/create it.  If it fails, the parent directory probably isn't writable.
-			if( !( $uploads = wp_upload_dir( null ) ) )
+			if( !( $uploads = wp_upload_dir( null ) ) ){
 				// return $uploads[ 'error' ];
+				#var_dump( array( '_media::wp_upload_dir::error' => $uploads[ 'error' ] ) );
 				return false;
+			}
 
 			// adjust file name for unique name
-			$filename = wp_unique_filename( $uploads[ 'path' ], $filename );			
+			$filename = wp_unique_filename( $uploads[ 'path' ], $filename );
+			
+			#var_dump( array( '_media::$filename' => $filename ) );
 
 			// full ath of new file
 			$new_file = $uploads[ 'path' ] .'/'. $filename;
+			
+			#var_dump( array( '_media::$new_file ' => $new_file  ) );
 
 			// Copy the uploaded file to the correct uploads folder.
-			if( false === @copy( $file, $new_file ) )
+			if( false === @copy( $file, $new_file ) ){
 				// return sprintf( __('The file %s could not be moved to %s.'), $file, $new_file );
+				#var_dump( array( '_media::copy::error ' => sprintf( __('The file %s could not be moved to %s.'), $file, $new_file ) ) );
 				return false;
+			}
 
 			// Set correct file permissions
 			$stat = stat( dirname( $new_file ) );
@@ -155,8 +177,10 @@ Date: 2015-02-28
 					wp_update_attachment_metadata( $att_id, $att_data );
 			
 					// return true;
-					return $uploads[ 'baseurl' ] .'/'. $att_data[ 'file' ];
+					#return $uploads[ 'baseurl' ] .'/'. $att_data[ 'file' ];
 				}
+				
+				return wp_get_attachment_url( $att_id );
 			}
 			return false;
 		}
